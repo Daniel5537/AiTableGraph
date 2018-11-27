9c5403d5b8bd2ac8abcf76527b551f81f20cf4be
dutao <1571397687@qq.com>
2018-11-27 11:48:04 +0800

    加上双引号

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -2,6 +2,7 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const commonConfig = require('./common');
+const {resolve} = require('path');
 
 exports.resolve =  function (relatedPath) {
     return path.join(__dirname, relatedPath)
@@ -13,7 +14,7 @@ module.exports = merge(commonConfig, {
     'react-hot-loader/patch', // activate HMR for React
     'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
     'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    './main.tsx' // the entry point of our app
+    resolve(__dirname, '../../src/main.tsx') // the entry point of our app
   ],
   devServer: {
     hot: true, // enable HMR on the server
